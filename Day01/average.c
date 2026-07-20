96aafc82dabdc3523dbf44d483ee636b5c20ddea
Bhavani-Codes-Source <muddangulabhavani20@gmail.com>
2026-07-20 09:57:45 +0530

    Create average.c

---
+++ Day01/average.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+  float num1,num2,num3,average;
+printf("Enter three numbes separated by space:");
+scanf("%f %f %f',&num1,&num2,&num3);
+  average=(num1+num2+num3)/3;
+  printf("The averageis:%.2f\n",average);
+return 0;
+}
